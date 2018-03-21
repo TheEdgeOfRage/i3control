a22822f7193affc5a0c862e865e547226c46f44e
Pavle Portic <pavle.portic@tilda.center>
2018-03-21 02:28:28 +0100

    Add backend

---
+++ backend/run.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from flask import Flask
+from subprocess import call
+
+
+app = Flask(__name__)
+
+
+@app.route('/mute')
+def mute():
+    call(['pactl', 'set-sink-mute', '0', 'toggle'])
+    return ''
+
+
+@app.route('/vol_down')
+def vol_down():
+    call(['pactl', 'set-sink-volume', '0', '-5%'])
+    return ''
+
+
+@app.route('/vol_up')
+def vol_up():
+    call(['pactl', 'set-sink-volume', '0', '+5%'])
+    return ''
+
+
+@app.route('/prev')
+def prev():
+    call(['cmus-remote', '-p'])
+    return ''
+
+
+@app.route('/play')
+def play():
+    call(['cmus-remote', '-u'])
+    return ''
+
+
+@app.route('/next')
+def next():
+    call(['cmus-remote', '-n'])
+    return ''
+
+
+if __name__ == '__main__':
+    app.run()
+
